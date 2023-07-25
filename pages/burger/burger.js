f43de0b2738810736e086dd4b175584935bcd1a4
Jake St. Germain <jakesaint27@gmail.com>
2023-07-24 23:34:26 -0400

    Finish up burger snippet

--- pages/burger/burger.js
+++ pages/burger/burger.js
@@ -1,6 +1,13 @@
-console.log(document.title);
 
-/*
-    TODO: make a false nav and show a hambuger menu
-        do not use the nav element -> explain why in comment
-*/
+const nav = document.getElementById("nav");
+const menuButton = document.querySelector(".screen button");
+
+showHideMenu = () => {
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+        menuButton.innerHTML = "|||";
+    } else {
+        nav.style.display = "block";
+        menuButton.innerHTML = "X";
+    }
+}
